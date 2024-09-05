96cb2450633266dd8da7aceeaa3b195eb50df570
mandar1jn <marijnkneppers@gmail.com>
2024-09-05 12:36:33 +0200

    start work on sector trailers

--- src/scenes/data.tsx
+++ src/scenes/data.tsx
@@ -1,5 +1,5 @@
-import { Circle, Code, lines, makeScene2D, Txt } from '@motion-canvas/2d';
-import { createRef, waitFor } from '@motion-canvas/core';
+import { Circle, Code, CodePoint, CodeRange, lines, makeScene2D, Txt } from '@motion-canvas/2d';
+import { all, createRef, waitFor } from '@motion-canvas/core';
 
 import whirlwindFigure from "../assets/figures/Whirlwind.bin?uint8array";
 
@@ -35,6 +35,37 @@ export default makeScene2D(function* (view) {
 	{
 		yield* text().selection(lines(i), 0.05);
 	}
+
+	yield* waitFor(0.5);
+
+	yield* text().selection(lines(0, 63), 0.5);
+
+	let sectorTrailers: CodeRange[] = []
+
+	for(let i = 0; i < 16; i++)
+	{
+		const selection = lines(i * 4 + 3);
+		sectorTrailers.push(selection);
+	}
+
+	yield* waitFor(0.75);
+
+	yield* text().selection(sectorTrailers, 0.5);
+
+	yield* all(
+		...[...Array(16).keys()].map(val => {
+			return text().code.remove(lines(val * 4, val * 4 + 2), 0.5);
+		}),
+		text().selection(lines(0, 15), 0.5)
+	);
+
+	yield* waitFor(0.75);
+
+	yield* all(
+		...[...Array(14).keys()].map(val => {
+			return text().code.remove(lines(val + 2), 0.75);
+		})
+	);
 
 	yield* waitFor(2);
 });
